ba702c2e100c03919914d940bdff02f78357dff7
Youngho Cho <dominoyh5@gmail.com>
2023-10-09 14:36:59 +0900

    main.cpp 에 parser 생성

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,5 @@ int main(int argc, char **argv) {
 	}
 
 	Lexer lexer(argv[1]);
-
-	Terminal next_token;
-	lexer.lexical();
-	next_token = lexer.get_next_token();
-	while (next_token != END_OF_FILE) {
-		cout << lexer.get_next_token() << ":\t";
-		cout << lexer.get_token_string() << '\n';
-		lexer.lexical();
-		next_token = lexer.get_next_token();
-	}
+	Parser parser(std::move(lexer));
 }
